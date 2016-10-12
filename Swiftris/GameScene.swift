b2f8d86fd3843118f371ebba97fc6089bebc031c
Nick Flege <nflege@iastate.edu>
2016-10-12 10:51:03 +0800

    Create project and add initial files

---
+++ Swiftris/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Swiftris
+//
+//  Created by Nick Flege on 10/12/16.
+//  Copyright © 2016 Nick Flege. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene
+{
+  override func update(_ currentTime: TimeInterval)
+  {
+    // Called before each frame is rendered
+  }
+}
---
+++ Swiftris/GameViewController.swift
@@ -0,0 +1,24 @@
+//
+//  GameViewController.swift
+//  Swiftris
+//
+//  Created by Nick Flege on 10/12/16.
+//  Copyright © 2016 Nick Flege. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController
+{
+  override func viewDidLoad()
+  {
+    super.viewDidLoad()
+  }
+
+  override var prefersStatusBarHidden: Bool
+  {
+    return true
+  }
+}
