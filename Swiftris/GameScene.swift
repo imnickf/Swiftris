400ff2f9aa116ec09a7ae9d0e1401090079a4d70
Nick Flege <nflege@iastate.edu>
2016-10-12 19:32:27 +0800

    Update image assets

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -11,6 +11,23 @@ import GameplayKit
 
 class GameScene: SKScene
 {
+  required init(coder aDecoder: NSCoder)
+  {
+    fatalError("NSCoder not supported")
+  }
+  
+  override init(size: CGSize)
+  {
+    super.init(size: size)
+    
+    anchorPoint = CGPoint(x: 0, y: 1.0)
+    
+    let background = SKSpriteNode(imageNamed: "background")
+    background.position = CGPoint(x: 0, y: 0)
+    background.anchorPoint = CGPoint(x: 0, y: 1)
+    addChild(background)
+  }
+  
   override func update(_ currentTime: TimeInterval)
   {
     // Called before each frame is rendered
