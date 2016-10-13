835bd7c20b3b6b2a8b9c4e633d7190cf0287de02
Nick Flege <nflege@iastate.edu>
2016-10-13 19:02:50 +0800

    Add logic to make shapes fall down screen

--- Swiftris/GameScene.swift
+++ Swiftris/GameScene.swift
@@ -10,12 +10,19 @@ import SpriteKit
 import GameplayKit
 
 let TickLengthLevelOne = TimeInterval(600)
+let BlockSize: CGFloat = 20.0
 
 class GameScene: SKScene
 {
+  let gameLayer = SKNode()
+  let shapeLayer = SKNode()
+  let LayerPosition = CGPoint(x: 6, y: -6)
+  
   var tick: (() -> ())?
   var tickLengthMillis = TickLengthLevelOne
   var lastTick: Date?
+  
+  var textureCache = [String : SKTexture]()
   
   required init(coder aDecoder: NSCoder)
   {
@@ -32,6 +39,16 @@ class GameScene: SKScene
     background.position = CGPoint(x: 0, y: 0)
     background.anchorPoint = CGPoint(x: 0, y: 1)
     addChild(background)
+    
+    addChild(gameLayer)
+    let gameBoardTexture = SKTexture(imageNamed: "gameboard")
+    let gameBoard = SKSpriteNode(texture: gameBoardTexture, size: CGSize(width: BlockSize * CGFloat(NumColumns), height: BlockSize * CGFloat(NumRows)))
+    gameBoard.anchorPoint = CGPoint(x: 0, y: 1.0)
+    gameBoard.position = LayerPosition
+    
+    shapeLayer.position = LayerPosition
+    shapeLayer.addChild(gameBoard)
+    gameLayer.addChild(shapeLayer)
   }
   
   override func update(_ currentTime: TimeInterval)
@@ -55,5 +72,65 @@ class GameScene: SKScene
   func stopTicking()
   {
     lastTick = nil
+  }
+  
+  func pointFor(column: Int, row: Int) -> CGPoint
+  {
+    let x = LayerPosition.x + (CGFloat(column) * BlockSize) + (BlockSize / 2)
+    let y = LayerPosition.y - ((CGFloat(row) * BlockSize) + (BlockSize / 2))
+    return CGPoint(x: x, y: y)
+  }
+  
+  func addPreviewShapeToScene(shape: Shape, completion: @escaping () -> ())
+  {
+    for block in shape.blocks {
+      var texture = textureCache[block.spriteName]
+      if texture == nil {
+        texture = SKTexture(imageNamed: block.spriteName)
+        textureCache[block.spriteName] = texture
+      }
+      
+      let sprite = SKSpriteNode(texture: texture)
+      sprite.position = pointFor(column: block.column, row: block.row - 2)
+      shapeLayer.addChild(sprite)
+      block.sprite = sprite
+      
+      sprite.alpha = 0
+      
+      let moveAction = SKAction.move(to: pointFor(column: block.column, row: block.row), duration: TimeInterval(0.2))
+      moveAction.timingMode = .easeOut
+      let fadeInAction = SKAction.fadeAlpha(to: 0.7, duration: 0.4)
+      fadeInAction.timingMode = .easeOut
+      sprite.run(SKAction.group([moveAction, fadeInAction]))
+    }
+    run(SKAction.wait(forDuration: 0.4), completion: completion)
+  }
+  
+  func movePreviewShape(shape: Shape, completion: @escaping () -> ())
+  {
+    for block in shape.blocks {
+      let sprite = block.sprite!
+      let moveTo = pointFor(column: block.column, row:block.row)
+      let moveToAction:SKAction = SKAction.move(to: moveTo, duration: 0.2)
+      moveToAction.timingMode = .easeOut
+      sprite.run(SKAction.group([moveToAction, SKAction.fadeAlpha(to: 1.0, duration: 0.2)]), completion: {})
+    }
+    run(SKAction.wait(forDuration: 0.2), completion: completion)
+  }
+  
+  func redrawShape(shape:Shape, completion: @escaping () -> ())
+  {
+    for block in shape.blocks {
+      let sprite = block.sprite!
+      let moveTo = pointFor(column: block.column, row:block.row)
+      let moveToAction:SKAction = SKAction.move(to: moveTo, duration: 0.05)
+      moveToAction.timingMode = .easeOut
+      
+      if block == shape.blocks.last {
+        sprite.run(moveToAction, completion: completion)
+      } else {
+        sprite.run(moveToAction)
+      }
+    }
   }
 }
